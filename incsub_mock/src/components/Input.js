fcb4b2a2a039f57387e38bf6bf973819fce2fa9d
jacobEliasMoya <49262410+jacobEliasMoya@users.noreply.github.com>
2021-08-08 16:52:40 -0700

    Delete incsub_mock directory

--- incsub_mock/src/components/Input.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Input(props) {
-    return (
-        <input onSubmit={props.submitform} value={props.inputValue} placeholder={props.inputPlaceholder} type={props.inputType} className={props.inputClass}/>
-    )
-}
--- incsub_mock/src/components/Paragraph.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Paragraph(props) {
-    return (
-        <p className={props.paraClass}>
-            {props.paragraphText}
-        </p>
-    )
-}
