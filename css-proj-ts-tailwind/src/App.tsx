702e6d6bc456f2733983cd24ed7d4ebed174c44b
jacob moya <jemoya1994@Gmail.com>
2021-08-10 21:45:08 -0700

    using hooks/conditionals to initialize app

--- css-proj-ts-tailwind/src/App.tsx
+++ css-proj-ts-tailwind/src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import MainContainer from './layout/MainContainer';
 import Nav from './layout/Nav';
 import IntroMenu from './layout/IntroMenu';
+import { useAppSelector } from './app/hooks';
+
 
 const App:React.FC = () => {
+
+  const stateBool = useAppSelector((state)=>state.appStart)
 
   return (
     <div className="App">
       <Nav/>
-      {/* {introMenu?<IntroMenu/>:<MainContainer/>} */}
+      {!stateBool?<IntroMenu/>:<MainContainer/>}
     </div>
   );
 }
