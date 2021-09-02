31cf5aebf685883e7dbe21150311f63e4ea15572
jacob moya <jemoya1994@Gmail.com>
2021-09-01 20:27:44 -0700

    added in state changes on click of buttons within nav to unrender the nav

--- css-proj-ts-tailwind/src/layout/MainNav.tsx
+++ css-proj-ts-tailwind/src/layout/MainNav.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { useAppSelector } from "../app/hooks";
 import Button from "../components/Button";
+import { useAppDispatch } from '../app/hooks';
+import { triggerNav } from '../store/features/MainNavSlice';
+
 const MainNav:React.FC = () =>{
 
     const cssStyles = useAppSelector(state=>state.cssImp);
 
+    const dispatch = useAppDispatch();
+
+    const handleClick = () => {
+        dispatch(triggerNav());
+    }
+
     return(
         <div className='navMenu'>
             {cssStyles.map(item=>{
-                return <Button buttonText={item.name} onclick={null}/>
+                return <Button buttonText={item.name} onclick={handleClick}/>
             })}
         </div>
     )
